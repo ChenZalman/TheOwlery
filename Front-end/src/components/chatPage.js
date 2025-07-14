e998f1f00a940510962687e6d95da26267456a6f
Tami Mesengiser <TamiM@feed-u.co>
2025-07-14 15:48:06 +0300

    t

--- Front-end/src/components/chatPage.js
+++ Front-end/src/components/chatPage.js
@@ -34,7 +34,6 @@ const Chat = () => {
 
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-    // console.log(messages)
   }, [messages]);
 
   const handleSend = (e) => {
